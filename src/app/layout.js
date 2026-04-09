2d1663a6b4452e028fc8b9beb7d5cbd4db677dff
shrimp316 <shrimpsushi12@gmail.com>
2026-04-09 20:06:02 +0900

    debug

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,11 +22,7 @@ export const metadata = {
   },
 };
 
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-};
+
 
 export default function RootLayout({ children }) {
   return (
